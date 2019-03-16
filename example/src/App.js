419c768d896eb5a103430663785172d79a6f9180
boggidan <sukmanov.b@gmail.com>
2019-03-16 15:04:57 +0100

    finishing example project + code format with semicolon and double "

--- example/src/App.js
+++ example/src/App.js
@@ -1,136 +1,327 @@
-import Typography from '@material-ui/core/Typography'
-import React, { Component } from 'react'
-
-import Autocomplete from 'react-autocomplete-suggestion'
+import { Paper, Typography, withStyles } from "@material-ui/core";
+import PropTypes from "prop-types";
+import React, { Component, Fragment } from "react";
+import Autocomplete from "react-autocomplete-suggestion";
 
 const initialSuggestions = [
-  { key: 0, label: 'Afghanistan' },
-  { key: 2, label: 'Aland Islands' },
-  { key: 3, label: 'Albania' },
-  { key: 4, label: 'Algeria' },
-  { key: 5, label: 'American Samoa' },
-  { key: 6, label: 'Andorra' },
-  { key: 7, label: 'Angola' },
-  { key: 8, label: 'Anguilla' },
-  { key: 9, label: 'Antarctica' },
-  { key: 10, label: 'Antigua and Barbuda' },
-  { key: 11, label: 'Argentina' },
-  { key: 12, label: 'Armenia' },
-  { key: 13, label: 'Aruba' },
-  { key: 14, label: 'Australia' },
-  { key: 15, label: 'Austria' },
-  { key: 16, label: 'Azerbaijan' },
-  { key: 17, label: 'Bahamas' },
-  { key: 18, label: 'Bahrain' },
-  { key: 19, label: 'Bangladesh' },
-  { key: 20, label: 'Barbados' },
-  { key: 21, label: 'Belarus' },
-  { key: 22, label: 'Belgium' },
-  { key: 23, label: 'Belize' },
-  { key: 24, label: 'Benin' },
-  { key: 25, label: 'Bermuda' },
-  { key: 26, label: 'Bhutan' },
-  { key: 27, label: 'Bolivia, Plurinational State of' },
-  { key: 28, label: 'Bonaire, Sint Eustatius and Saba' },
-  { key: 29, label: 'Bosnia and Herzegovina' },
-  { key: 30, label: 'Botswana' },
-  { key: 31, label: 'Bouvet Island' },
-  { key: 32, label: 'Brazil' },
-  { key: 33, label: 'British Indian Ocean Territory' },
-  { key: 34, label: 'Brunei Darussalam' }
-]
+  { key: 0, label: "Afghanistan" },
+  { key: 2, label: "Aland Islands" },
+  { key: 3, label: "Albania" },
+  { key: 4, label: "Algeria" },
+  { key: 5, label: "American Samoa" },
+  { key: 6, label: "Andorra" },
+  { key: 7, label: "Angola" },
+  { key: 8, label: "Anguilla" },
+  { key: 9, label: "Antarctica" },
+  { key: 10, label: "Antigua and Barbuda" },
+  { key: 11, label: "Argentina" },
+  { key: 12, label: "Armenia" },
+  { key: 13, label: "Aruba" },
+  { key: 14, label: "Australia" },
+  { key: 15, label: "Austria" },
+  { key: 16, label: "Azerbaijan" },
+  { key: 17, label: "Bahamas" },
+  { key: 18, label: "Bahrain" },
+  { key: 19, label: "Bangladesh" },
+  { key: 20, label: "Barbados" },
+  { key: 21, label: "Belarus" },
+  { key: 22, label: "Belgium" },
+  { key: 23, label: "Belize" },
+  { key: 24, label: "Benin" },
+  { key: 25, label: "Bermuda" },
+  { key: 26, label: "Bhutan" },
+  { key: 27, label: "Bolivia, Plurinational State of" },
+  { key: 28, label: "Bonaire, Sint Eustatius and Saba" },
+  { key: 29, label: "Bosnia and Herzegovina" },
+  { key: 30, label: "Botswana" },
+  { key: 31, label: "Bouvet Island" },
+  { key: 32, label: "Brazil" },
+  { key: 33, label: "British Indian Ocean Territory" },
+  { key: 34, label: "Brunei Darussalam" }
+];
+
+const styles = () => (
+  {
+    header: {
+      height: "auto",
+      textAlign: "center",
+      backgroundColor: "#37C0C8",
+      padding: 20,
+      marginBottom: 20
+    },
+
+    headerText: {
+      color: "#FFFFFF"
+    },
+
+    container: {
+      display: "flex",
+      padding: 10
+    },
+
+    contentContainer: {
+      width: "65%",
+      height: 780,
+      padding: 10,
+      marginRight: 5
+    },
+
+    logsContainer: {
+      width: "35%",
+      maxHeight: 780,
+      overflow: "auto",
+      padding: 10,
+      marginLeft: 5
+    }
+  }
+);
 
 class App extends Component {
-  state = {
-    suggestions: [],
-    value: ''
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      suggestions: [],
+      value: "",
+      logKey: 0,
+      logEntries: ""
+    };
+    this.scrollBottom = React.createRef();
   }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.scrollLogsToBottom();
+  }
+
+  scrollLogsToBottom = () => {
+    this.scrollBottom.current.scrollIntoView({
+      block: "end",
+      inline: "nearest",
+      behavior: "smooth"
+    });
+  };
 
   fetchItems = value => {
-    const inputValue = value.trim().toLowerCase()
-    const inputLength = inputValue.length
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+    const newLogKey = this.state.logKey + 1;
 
-    this.setState({
-      suggestions: inputLength === 0 ? [] : initialSuggestions.filter(country =>
-        country.label.toLowerCase().slice(0, inputLength) === inputValue)
-    })
-  }
+    this.setState(state => (
+      {
+        suggestions: inputLength === 0 ? [] : initialSuggestions.filter(
+          country => country.label.toLowerCase().slice(0, inputLength) === inputValue
+        ),
+        logKey: newLogKey,
+        logEntries: [
+          ...state.logEntries,
+          <p key={newLogKey}> > On fetch suggestions event called with value: {value}</p>
+        ]
+      }
+    ));
+  };
 
   onChange = (event, value) => {
-    this.setState({ value: value })
-  }
+    const newLogKey = this.state.logKey + 1;
 
-  onSuggestionSelected = suggestion => {}
+    this.setState(state => (
+      {
+        value: value,
+        logKey: newLogKey,
+        logEntries: [
+          ...state.logEntries,
+          <p key={newLogKey}> > On change event called with value: {value}</p>
+        ]
+      }
+    ));
+  };
 
-  onExactMatchFound = exactMatchSuggestion => {}
+  onSuggestionSelected = suggestion => {
+    const newLogKey = this.state.logKey + 1;
 
-  onBlur = event => {}
+    this.setState(state => (
+      {
+        logKey: newLogKey,
+        logEntries: [
+          ...state.logEntries,
+          <p key={newLogKey}> > On suggestion selected event called with label: {suggestion.label} and
+            key: {suggestion.key}</p>
+        ]
+      }
+    ));
+  };
 
-  onFocus = event => {}
+  onExactMatchFound = exactMatchSuggestion => {
+    const newLogKey = this.state.logKey + 1;
+
+    this.setState(state => (
+      {
+        logKey: newLogKey,
+        logEntries: [
+          ...state.logEntries,
+          <p key={newLogKey}> > On exact match event called with label: {exactMatchSuggestion.label} and
+            key: {exactMatchSuggestion.key}`</p>
+        ]
+      }
+    ));
+  };
+
+  onBlur = event => {
+    const newLogKey = this.state.logKey + 1;
+
+    this.setState(state => (
+      {
+        logKey: newLogKey,
+        logEntries: [
+          ...state.logEntries,
+          <p key={newLogKey}> > On blur event called</p>
+        ]
+      }
+    ));
+  };
+
+  onFocus = event => {
+    const newLogKey = this.state.logKey + 1;
+
+    this.setState(state => (
+      {
+        logKey: newLogKey,
+        logEntries: [
+          ...state.logEntries,
+          <p key={newLogKey}> > On focus event called</p>
+        ]
+      }
+    ));
+  };
 
   render() {
-    const { suggestions, value, helperText } = this.state
+    const { classes } = this.props;
+    const { suggestions, value, logEntries } = this.state;
 
     return (
+      <Fragment>
+        <div className={classes.header}>
+          <Typography className={classes.headerText} variant="h1">react-autocomplete-suggestion example</Typography>
+        </div>
+        <div className={classes.container}>
+          <Paper className={classes.contentContainer}>
+            <Typography variant="title">Autocomplete with custom theme</Typography>
+            <Autocomplete
+              placeholder="Type 'a' or 'b'"
+              label="country"
+              suggestions={suggestions}
+              value={value}
+              onFetchSuggestions={this.fetchItems}
+              onChange={this.onChange}
+              onSuggestionSelected={this.onSuggestionSelected}
+              onExactMatchFound={this.onExactMatchFound}
+              onBlur={this.onBlur}
+              onFocus={this.onFocus}
+              theme={{
+                suggestionHighlighted: {
+                  backgroundColor: "yellow"
+                },
 
-      <div className="App">
-        <Typography variant="h3">Free text disabled and ignore case enabled</Typography>
-        <Autocomplete
-          placeholder="Search for a country"
-          label="country"
-          suggestions={suggestions}
-          value={value}
-          onFetchSuggestions={this.fetchItems}
-          onChange={this.onChange}
-          onSuggestionSelected={this.onSuggestionSelected}
-          onExactMatchFound={this.onExactMatchFound}
-          onBlur={this.onBlur}
-          onFocus={this.onFocus}
-          isInError={!!helperText}
-          helperText={helperText}
-          theme={{
-            suggestionHighlighted: {
-              backgroundColor: 'yellow'
-            },
+                highlight: {
+                  fontWeight: "none",
+                  color: "red"
+                }
+              }}
+            />
+            <Typography variant="title">Autocomplete in error</Typography>
+            <Autocomplete
+              placeholder="Type 'a' or 'b'"
+              label="country"
+              suggestions={suggestions}
+              value={value}
+              onFetchSuggestions={this.fetchItems}
+              onChange={this.onChange}
+              onSuggestionSelected={this.onSuggestionSelected}
+              onExactMatchFound={this.onExactMatchFound}
+              onBlur={this.onBlur}
+              onFocus={this.onFocus}
+              isInError
+              helperText="An error occurred"
+            />
+            <Typography variant="title">Free text disabled and ignore case enabled</Typography>
+            <Typography>The value will be cleared if the user does not select it by click, enter or tab.</Typography>
+            <Typography>An exact match can be found with <b>case insensitive</b> matching.</Typography>
+            <Autocomplete
+              placeholder="Type 'a' or 'b'"
+              label="country"
+              suggestions={suggestions}
+              value={value}
+              onFetchSuggestions={this.fetchItems}
+              onChange={this.onChange}
+              onSuggestionSelected={this.onSuggestionSelected}
+              onExactMatchFound={this.onExactMatchFound}
+              onBlur={this.onBlur}
+              onFocus={this.onFocus}
+            />
 
-            highlight: {
-              fontWeight: 'none',
-              color: 'red'
-            }
-          }}
-        />
-        <Typography variant="h3">Free text enabled and ignore case enabled</Typography>
-        <Autocomplete
-          placeholder="Search for a country"
-          label="country"
-          freeTextEnabled
-          suggestions={suggestions}
-          value={value}
-          onFetchSuggestions={this.fetchItems}
-          onChange={this.onChange}
-          onSuggestionSelected={this.onSuggestionSelected}
-          onExactMatchFound={this.onExactMatchFound}
-          onBlur={this.onBlur}
-          onFocus={this.onFocus}
-        />
-        <Typography variant="h3">Free text enabled and ignore case disabled</Typography>
-        <Autocomplete
-          placeholder="Search for a country"
-          label="country"
-          freeTextEnabled
-          ignoreCase={false}
-          suggestions={suggestions}
-          value={value}
-          onFetchSuggestions={this.fetchItems}
-          onChange={this.onChange}
-          onSuggestionSelected={this.onSuggestionSelected}
-          onExactMatchFound={this.onExactMatchFound}
-          onBlur={this.onBlur}
-          onFocus={this.onFocus}
-        />
-      </div>
+            <Typography variant="title">Free text disabled and ignore case disabled</Typography>
+            <Typography>The value will be cleared if the user does not select it by click, enter or tab.</Typography>
+            <Typography>An exact match can be found with <b>case sensitive</b> matching.</Typography>
+            <Autocomplete
+              placeholder="Type 'a' or 'b'"
+              label="country"
+              ignoreCase={false}
+              suggestions={suggestions}
+              value={value}
+              onFetchSuggestions={this.fetchItems}
+              onChange={this.onChange}
+              onSuggestionSelected={this.onSuggestionSelected}
+              onExactMatchFound={this.onExactMatchFound}
+              onBlur={this.onBlur}
+              onFocus={this.onFocus}
+            />
+
+            <Typography variant="title">Free text enabled and ignore case enabled</Typography>
+            <Typography>The value won't be cleared if the user does not select it by click, enter or tab.</Typography>
+            <Typography>An exact match can be found with <b>case insensitive</b> matching.</Typography>
+            <Autocomplete
+              placeholder="Type 'a' or 'b'"
+              label="country"
+              freeTextEnabled
+              suggestions={suggestions}
+              value={value}
+              onFetchSuggestions={this.fetchItems}
+              onChange={this.onChange}
+              onSuggestionSelected={this.onSuggestionSelected}
+              onExactMatchFound={this.onExactMatchFound}
+              onBlur={this.onBlur}
+              onFocus={this.onFocus}
+            />
+            <Typography variant="title">Free text enabled and ignore case disabled</Typography>
+            <Typography>The value won't be cleared if the user does not select it by click, enter or tab.</Typography>
+            <Typography>An exact match can be found with <b>case sensitive</b> matching.</Typography>
+            <Autocomplete
+              placeholder="Type 'a' or 'b'"
+              label="country"
+              freeTextEnabled
+              ignoreCase={false}
+              suggestions={suggestions}
+              value={value}
+              onFetchSuggestions={this.fetchItems}
+              onChange={this.onChange}
+              onSuggestionSelected={this.onSuggestionSelected}
+              onExactMatchFound={this.onExactMatchFound}
+              onBlur={this.onBlur}
+              onFocus={this.onFocus}
+            />
+          </Paper>
+          <Paper className={classes.logsContainer}>
+            <Typography variant="title">Logs</Typography>
+            {logEntries}
+            <div ref={this.scrollBottom}/>
+          </Paper>
+        </div>
+      </Fragment>
     );
   }
 }
 
-export default App
+Autocomplete.PropTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -1,192 +1,192 @@
-import { Paper, TextField, Typography } from '@material-ui/core'
-import AutosuggestHighlightMatch from 'autosuggest-highlight/match'
-import AutosuggestHighlightParse from 'autosuggest-highlight/parse'
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import Autosuggest from 'react-autosuggest'
-import { validKeys } from './contants'
-import { constructCustomTheme } from './defaultTheme'
+import { Paper, TextField, Typography } from "@material-ui/core";
+import AutosuggestHighlightMatch from "autosuggest-highlight/match";
+import AutosuggestHighlightParse from "autosuggest-highlight/parse";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import Autosuggest from "react-autosuggest";
+import { validKeys } from "./contants";
+import { constructCustomTheme } from "./defaultTheme";
 
-window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true
+window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 
 class Autocomplete extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    const { value, theme } = props
+    const { value, theme } = props;
 
     this.state = {
       fetchTimeout: null,
-      value: value || '',
+      value: value ? value : "",
       selectedSuggestion: null,
       exactMatchSuggestion: null,
       lastPressedKey: null,
       theme: constructCustomTheme(theme)
-    }
+    };
   }
 
   static getDerivedStateFromProps(props, state) {
-    const { onExactMatchFound, suggestions, ignoreCase } = props
-    const { exactMatchSuggestion, value } = state
-    const newExactMatchSuggestion = Autocomplete.getExactMatchSuggestion(suggestions, value, ignoreCase)
+    const { onExactMatchFound, suggestions, ignoreCase } = props;
+    const { exactMatchSuggestion, value } = state;
+    const newExactMatchSuggestion = Autocomplete.getExactMatchSuggestion(suggestions, value, ignoreCase);
 
     if (newExactMatchSuggestion && exactMatchSuggestion !== newExactMatchSuggestion) {
-      onExactMatchFound && onExactMatchFound(newExactMatchSuggestion)
+      onExactMatchFound && onExactMatchFound(newExactMatchSuggestion);
 
       return {
         exactMatchSuggestion: newExactMatchSuggestion
-      }
+      };
     }
 
-    return null
+    return null;
   }
 
   static formatValue(value, ignoreCase) {
-    return ignoreCase ? value.trim().toLocaleLowerCase() : value.trim()
+    return ignoreCase ? value.trim().toLocaleLowerCase() : value.trim();
   };
 
   static isValueEqualsToSuggestion(suggestion, value, ignoreCase) {
-    return Autocomplete.formatValue(suggestion.label, ignoreCase) === Autocomplete.formatValue(value, ignoreCase)
+    return Autocomplete.formatValue(suggestion.label, ignoreCase) === Autocomplete.formatValue(value, ignoreCase);
   };
 
   static getExactMatchSuggestion(suggestions, value, ignoreCase) {
-    if (!value || suggestions.length <= 0) return null
+    if (!value || suggestions.length <= 0) return null;
 
-    return suggestions.find(suggestion => Autocomplete.isValueEqualsToSuggestion(suggestion, value, ignoreCase))
+    return suggestions.find(suggestion => Autocomplete.isValueEqualsToSuggestion(suggestion, value, ignoreCase));
   };
 
   hasExactMatch = () => {
-    const { exactMatchSuggestion } = this.state
+    const { exactMatchSuggestion } = this.state;
 
-    return !!exactMatchSuggestion
-  }
+    return !!exactMatchSuggestion;
+  };
 
   isValidPressedKey(pressedKey) {
-    return validKeys.includes(pressedKey)
+    return validKeys.includes(pressedKey);
   }
 
   shouldValueBeClearedOnKeyDown = pressedKey => {
-    const { freeTextEnabled } = this.props
+    const { freeTextEnabled } = this.props;
 
-    return freeTextEnabled ? false : this.isValidPressedKey(pressedKey) && !this.hasExactMatch()
-  }
+    return freeTextEnabled ? false : this.isValidPressedKey(pressedKey) && !this.hasExactMatch();
+  };
 
   shouldValueBeClearedOnBlur = () => {
-    const { lastPressedKey } = this.state
-    const { freeTextEnabled } = this.props
+    const { lastPressedKey } = this.state;
+    const { freeTextEnabled } = this.props;
 
-    return !freeTextEnabled && !this.isValidPressedKey(lastPressedKey)
-  }
+    return !freeTextEnabled && !this.isValidPressedKey(lastPressedKey);
+  };
 
-  getSuggestionValue = suggestion => suggestion.label
+  getSuggestionValue = suggestion => suggestion.label;
 
   onSuggestionsFetchRequested = ({ value }) => {
-    const { onFetchSuggestions, fetchTimeoutTimer } = this.props
+    const { onFetchSuggestions, fetchTimeoutTimer } = this.props;
 
     if (onFetchSuggestions) {
-      this.state.fetchTimeoutTimer && clearTimeout(this.state.fetchTimeoutTimer)
+      this.state.fetchTimeoutTimer && clearTimeout(this.state.fetchTimeoutTimer);
       this.setState({
         fetchTimeoutTimer: setTimeout(() => {
-          onFetchSuggestions(value)
+          onFetchSuggestions(value);
         }, fetchTimeoutTimer)
-      })
+      });
     }
-  }
+  };
 
   onSuggestionsClearRequested = () => {
-    const { onSuggestionsClearRequested } = this.props
+    const { onSuggestionsClearRequested } = this.props;
 
-    onSuggestionsClearRequested && onSuggestionsClearRequested()
-  }
+    onSuggestionsClearRequested && onSuggestionsClearRequested();
+  };
 
   onSuggestionSelected = (event, { suggestion }) => {
-    const { onSuggestionSelected } = this.props
-    const { selectedSuggestion } = this.state
+    const { onSuggestionSelected } = this.props;
+    const { selectedSuggestion } = this.state;
 
     if (suggestion !== selectedSuggestion) {
       this.setState({
         value: suggestion.label,
         selectedSuggestion: suggestion
-      })
-      onSuggestionSelected && onSuggestionSelected(suggestion)
+      });
+      onSuggestionSelected && onSuggestionSelected(suggestion);
     }
-  }
+  };
 
   onChange = (event, { newValue }) => {
-    const { onChange } = this.props
+    const { onChange } = this.props;
 
     this.setState({
       value: newValue,
       selectedSuggestion: null,
       exactMatchSuggestion: null,
       lastPressedKey: null
-    })
-    onChange && onChange(event, newValue)
-  }
+    });
+    onChange && onChange(event, newValue);
+  };
 
   onKeyDown = event => {
-    this.setState({ lastPressedKey: event.key })
+    this.setState({ lastPressedKey: event.key });
 
     if (this.shouldValueBeClearedOnKeyDown(event.key)) {
-      event.preventDefault()
-      this.onChange(event, { newValue: '' })
+      event.preventDefault();
+      this.onChange(event, { newValue: "" });
     } else if (this.isValidPressedKey(event.key)) {
-      const { exactMatchSuggestion } = this.state
+      const { exactMatchSuggestion } = this.state;
 
-      exactMatchSuggestion && this.onSuggestionSelected(event, { suggestion: exactMatchSuggestion })
+      exactMatchSuggestion && this.onSuggestionSelected(event, { suggestion: exactMatchSuggestion });
     }
-  }
+  };
 
   onBlur = event => {
-    const { onBlur } = this.props
+    const { onBlur } = this.props;
 
     if (this.shouldValueBeClearedOnBlur()) {
-      this.onChange(event, { newValue: '' })
+      this.onChange(event, { newValue: "" });
     }
 
-    onBlur && onBlur(event)
-  }
+    onBlur && onBlur(event);
+  };
 
   onFocus = event => {
-    const { onFocus } = this.props
+    const { onFocus } = this.props;
 
-    onFocus && onFocus(event)
-  }
+    onFocus && onFocus(event);
+  };
 
   renderHighlights = (part, index) => {
-    const { theme } = this.state
-    const className = part.highlight ? theme.highlight : null
+    const { theme } = this.state;
+    const className = part.highlight ? theme.highlight : null;
 
     return (
       <span key={index} style={{ ...className }}>
         {part.text}
       </span>
-    )
-  }
+    );
+  };
 
   renderSuggestion = (suggestion, { query }) => {
-    const { label } = suggestion
-    const matches = AutosuggestHighlightMatch(label, query)
-    const parts = AutosuggestHighlightParse(label, matches)
+    const { label } = suggestion;
+    const matches = AutosuggestHighlightMatch(label, query);
+    const parts = AutosuggestHighlightParse(label, matches);
 
-    return <Typography noWrap>{parts.map(this.renderHighlights)}</Typography>
-  }
+    return <Typography noWrap>{parts.map(this.renderHighlights)}</Typography>;
+  };
 
   renderSuggestionsContainer = options => {
     return (
       <Paper {...options.containerProps} square>
         {options.children}
       </Paper>
-    )
-  }
+    );
+  };
 
   renderInputComponent = inputProps => {
-    return <TextField fullWidth margin="normal" {...inputProps}/>
+    return <TextField fullWidth margin="normal" {...inputProps}/>;
   };
 
   render() {
-    const props = this.props
-    const { value, theme } = this.state
+    const props = this.props;
+    const { value, theme } = this.state;
 
     const inputProps = {
       placeholder: props.placeholder,
@@ -198,7 +198,7 @@ class Autocomplete extends Component {
       onBlur: this.onBlur,
       onFocus: this.onFocus,
       onKeyDown: this.onKeyDown
-    }
+    };
 
     const autosuggestProps = {
       inputProps,
@@ -211,7 +211,7 @@ class Autocomplete extends Component {
       onSuggestionsClearRequested: this.onSuggestionsClearRequested,
       onSuggestionSelected: this.onSuggestionSelected,
       getSuggestionValue: this.getSuggestionValue
-    }
+    };
 
     return (
       <Autosuggest
@@ -259,8 +259,8 @@ Autocomplete.propTypes = {
       key: PropTypes.any.isRequired,
       label: PropTypes.string.isRequired
     })
-  ).isRequired
-}
+  )
+};
 
 Autocomplete.defaultProps = {
   suggestions: [],
@@ -271,9 +271,9 @@ Autocomplete.defaultProps = {
 
   isInError: false,
 
-  helperText: '',
+  helperText: "",
 
   fetchTimeoutTimer: 200
-}
+};
 
-export default Autocomplete
+export default Autocomplete;
